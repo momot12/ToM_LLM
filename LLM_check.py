8a4180312cfd561a5388c711abdf66a7aec4ab5d
Momo Takamatsu <takamamo@kiwi.ims.uni-stuttgart.de>
2025-01-29 15:25:01 +0100

    trials

--- LLM_check.py
+++ LLM_check.py
@@ -3,6 +3,9 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f'DEVICE: {device}')
+torch.cuda.empty_cache()
 
 cache_dir = "/mount/studenten/arbeitsdaten-studenten4/team-lab-cl/data2024/tom_project/hf_cache"
 
@@ -14,18 +17,13 @@ model = AutoModelForCausalLM.from_pretrained("tiiuae/falcon-7b-instruct", trust_
 
 model_name = "tiiuae/falcon-7b-instruct"
 
-# Move model to GPU if available
-device = "cuda" if torch.cuda.is_available() else "cpu"
-print(f'DEVICE: {device}')
-torch.cuda.empty_cache()
-
 model.to(device)
 
 # Function to generate text from a prompt
 def generate_response(prompt, max_length=200):
     inputs = tokenizer(prompt, return_tensors="pt").to(device)  # Tokenize input and move to device
     with torch.no_grad():  # No gradient calculation for inference
-        outputs = model.generate(**inputs, max_length=max_length, do_sample=True, temperature=0.7, top_p=0.9)
+        outputs = model.generate(**inputs, max_length=max_length, do_sample=False, temperature=0.0, top_p=0.9)
     
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
 
